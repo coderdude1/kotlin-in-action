f339861e036fb765df18853edd9eb66a916b67c8
Randy <drbugsmn@gmail.com>
2017-07-18 18:34:22 -0500

    added some tweeks to experiment with mutability on the properties exercise

--- src/ch02/2.2.1_2_Properties1.kt
+++ src/ch02/2.2.1_2_Properties1.kt
@@ -1,12 +1,16 @@
 package ch02
 
 class Person(
-    val name: String,
-    var isMarried: Boolean
+    val name: String,   //Immutable
+    var isMarried: Boolean //mutable
 )
 
 fun main(args: Array<String>) {
-    val person = Person("Bob", true)
+    val person = Person("john", true)
     println(person.name)
     println(person.isMarried)
+    person.isMarried = false
+//    person = Person("blargh", true)  //compile/edit time warning about val cannot be reassigned
+
+//    person.name = 'blargh' //compile/edit time warning about val cannot be reassigned
 }
