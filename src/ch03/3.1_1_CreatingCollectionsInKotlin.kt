9265eb74cfc16bfcf2589060be813a05559aa99a
Randy <drbugsmn@gmail.com>
2017-08-15 22:32:11 -0500

    intro to some very basic colletion stuff

--- src/ch03/3.1_1_CreatingCollectionsInKotlin.kt
+++ src/ch03/3.1_1_CreatingCollectionsInKotlin.kt
@@ -1,11 +1,20 @@
 package ch03.ex1_1_CreatingCollectionsInKotlin
 
-val set = hashSetOf(1, 7, 53)
+val set = hashSetOf(1, 7, 53) //some kotlin convience methods note no static import kotlin collections
 val list = arrayListOf(1, 7, 53)
 val map = hashMapOf(1 to "one", 7 to "seven", 53 to "fifty-three")
 
 fun main(args: Array<String>) {
-    println(set.javaClass)
+    println(set.javaClass) //same as set.getClass() in java, prints java.util.HashSet
     println(list.javaClass)
     println(map.javaClass)
+
+    println(set)
+    println(list)
+    println(map)
 }
+/*
+kotlin does not define it's own collections it uses the standard java collections
+ although kotlin adds some nice stuff like the last, max etc, etc.  more in the next example
+look in the 3.1_2 for examples
+ */
--- src/ch03/3.1_2_CreatingCollectionsInKotlin1.kt
+++ src/ch03/3.1_2_CreatingCollectionsInKotlin1.kt
@@ -2,7 +2,9 @@ package ch03.ex1_2_CreatingCollectionsInKotlin1
 
 fun main(args: Array<String>) {
     val strings = listOf("first", "second", "fourteenth")
+    println(strings.javaClass) //arrayList
     println(strings.last())
     val numbers = setOf(1, 14, 2)
+    println(numbers.javaClass) //LinkedHashSet
     println(numbers.max())
 }
