bf585a71b167c15247a4a28b3952d1bb0ca8d084
Randy <drbugsmn@gmail.com>
2017-09-11 22:12:35 -0500

    more figuring out how extension functions can't be overriden via inheritance

--- src/ch03/3.3.4_2_NoOverridingForExtensionFunctions1.kt
+++ src/ch03/3.3.4_2_NoOverridingForExtensionFunctions1.kt
@@ -4,17 +4,23 @@ open class View {   //open means open for extesnion
     open fun click() = println("View clicked")
 }
 
-class Button: View() {  //default private (ie not open)
+class Button : View() {  //default private (ie not open)
     override fun click() = println("Button clicked")
 }
 
-fun View.showOff() = println("I'm a view!") //extension function on View
-fun Button.showOff() = println("I'm a button!") //extension function on Button (not an override!)
+fun View.showOff() = println("I'm a view!") //extension function on View.  Intellij warning 'receiver param is never used'
+fun Button.showOff() = println("I'm a button!") //extension function on Button (not an override!)  Intellij warning 'receiver param is never used'
 
 fun main(args: Array<String>) {
     val view: View = Button()  //button extends view
     view.showOff()  //"Im a view!" note not button cannot override an extension, thus button.view is not called
     view.click() //"button clicked"
 
-    //extension functions are not part of the class, they appear to be compiled to static fn in java
+    /*extension functions are not part of the class, they appear to be compiled to static fn in a extension java class,
+    looking in the build dir I found this file
+    ex3_4_2_NoOverridingForExtensionFunctions1Kt.class and it has this function
+        public fun ch03.ex3_4_2_NoOverridingForExtensionFunctions1.View.showOff(): kotlin.Unit { /* compiled code */ }
+
+    Note member functions always override extension functions if the fn names are the same.
+    */
 }
