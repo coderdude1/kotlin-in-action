6afa4a5b0c279a22d35445958f14300a1f92c186
Randy <drbugsmn@gmail.com>
2017-09-17 20:38:10 -0500

    infix

--- src/ch03/3.4.2_Varargs.kt
+++ src/ch03/3.4.2_Varargs.kt
@@ -1,6 +1,22 @@
 package ch03.ex4_2_Varargs
 
 fun main(args: Array<String>) {
-    val list = listOf("args: ", *args)
+    val list = listOf("args: ", *args) //* is spreadOperator, unpacks the list of args
     println(list)
+
+    //the word 'to' is a fn invocation called an infix call to create a map
+    // an infix call is a seperator between the parameter and the target object
+    //you can create an infix fn by adding the keyword infix to it
+    //https://kotlinlang.org/docs/reference/functions.html (search for infix)
+    //note it returns a Pair (in this case a key/value of a map?)
+    val mapOfStuff = mapOf(1 to "one", 7 to "seven", 53 to "fifty-three")
+    println(mapOfStuff)
+
+    //destructuring declaration, init two vars with a pair (of infix)
+    val (number, name) = 1 to "one"
+    println(number)
+    println(name)
+
+    //ex
+//    for ((index, element) in collection.withIndex()) {     println("$index: $element") }
 }
