299a011a2bc77338126d52f3292a6095c5181889
Randy <drbugsmn@gmail.com>
2017-09-17 20:53:17 -0500

    Splititng strings examples

--- src/ch03/3.5.1_2_SplittingStrings1.kt
+++ src/ch03/3.5.1_2_SplittingStrings1.kt
@@ -1,5 +1,5 @@
 package ch03.ex5_1_2_SplittingStrings1
 
 fun main(args: Array<String>) {
-    println("12.345-6.A".split(".", "-"))
+    println("12.345-6.A".split(".", "-"))//kotlin style split notice no regex due to kotlin overload on split
 }
