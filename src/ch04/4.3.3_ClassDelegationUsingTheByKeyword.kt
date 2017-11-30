e437de44746425e2370cddf94f743536eeece4bc
Randy <drbugsmn@gmail.com>
2017-11-29 19:42:42 -0600

    delegate using the 'by' keyword

--- src/ch04/4.3.3_ClassDelegationUsingTheByKeyword.kt
+++ src/ch04/4.3.3_ClassDelegationUsingTheByKeyword.kt
@@ -2,18 +2,21 @@ package ch04.ex3_3_ClassDelegationUsingTheByKeyword
 
 import java.util.HashSet
 
+//classes are final by default
 class CountingSet<T>(
-        val innerSet: MutableCollection<T> = HashSet<T>()
-) : MutableCollection<T> by innerSet {
+        val innerSet: MutableCollection<T> = HashSet<T>()  //declare a innerSert var, not sure what the mutableCollection does yet
+) : MutableCollection<T> by innerSet { //delegates the MutableCollection impl to innerset (like the decpratpr pattern
 
     var objectsAdded = 0
 
-    override fun add(element: T): Boolean {
+    //all other MutableCollection methods are available on innerSet but delegated to innerSet
+    // we just override 2 of them
+    override fun add(element: T): Boolean { //override the MutableCollection add, ie do not delegate
         objectsAdded++
         return innerSet.add(element)
     }
 
-    override fun addAll(c: Collection<T>): Boolean {
+    override fun addAll(c: Collection<T>): Boolean { //override the MutableCollection.addAll
         objectsAdded += c.size
         return innerSet.addAll(c)
     }
