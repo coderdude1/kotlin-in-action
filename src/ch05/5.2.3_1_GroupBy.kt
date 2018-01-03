199c1376bb3c5c5884ff09b881e82345eddd5e55
Randy <drbugsmn@gmail.com>
2018-01-03 10:48:05 -0600

    collection extension groupBy

--- src/ch05/5.2.3_1_GroupBy.kt
+++ src/ch05/5.2.3_1_GroupBy.kt
@@ -5,5 +5,7 @@ data class Person(val name: String, val age: Int)
 fun main(args: Array<String>) {
     val people = listOf(Person("Alice", 31),
             Person("Bob", 29), Person("Carol", 31))
+    //simlar to java partitionBy, behaves like a multimap, in this case returns a map keyed by age,
+    // with a list of People that matches the predicate
     println(people.groupBy { it.age })
 }
--- src/ch05/5.2.3_2_GroupBy1.kt
+++ src/ch05/5.2.3_2_GroupBy1.kt
@@ -2,5 +2,9 @@ package ch05.ex2_3_2_GroupBy1
 
 fun main(args: Array<String>) {
     val list = listOf("a", "ab", "b")
+    /*
+    first is an extension method on String but can be used as a member reference
+    result is map with the first letter as the key, the value is a list that has that letter as first
+     */
     println(list.groupBy(String::first))
 }
