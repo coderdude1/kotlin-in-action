d2122f51a8cec501979fb1f568f17a40f429b5d6
Randy <drbugsmn@gmail.com>
2018-01-05 20:28:00 -0600

    creating sequnces of stuff, including file/dir structures

--- src/ch05/5.3.2_1_CreatingSequences.kt
+++ src/ch05/5.3.2_1_CreatingSequences.kt
@@ -1,7 +1,9 @@
 package ch05.ex3_2_1_CreatingSequences
 
 fun main(args: Array<String>) {
+    //generateSequence is a kotlin func, arg is seed lambda is incementing func
     val naturalNumbers = generateSequence(0) { it + 1 }
+    //returns a new seq based on the original, bound by the predicate in takeWhile
     val numbersTo100 = naturalNumbers.takeWhile { it <= 100 }
-    println(numbersTo100.sum())
+    println(numbersTo100.sum())//terminal op triggers all operations to go
 }
--- src/ch05/5.3.2_2_CreatingSequences1.kt
+++ src/ch05/5.3.2_2_CreatingSequences1.kt
@@ -2,6 +2,18 @@ package ch05.ex3_2_2_CreatingSequences1
 
 import java.io.File
 
+/**
+ * Using a sequence to walk a directory tree.  can replace .any with .fihnd to look for something
+ * specific
+ *
+ * I think ocnce it finds a hidden dir it
+ *
+ * I need to work with this more to understand it better
+ *
+ * this is an exntensino function on java File, so 'this' will refer to the file defined in the main function
+ * the it.parentFile will map to the getParentFile method on the java File instance
+ * any(it.isHiddden) returns if the predicate is matched.  .HiddenDir will return true
+ * */
 fun File.isInsideHiddenDirectory() =
         generateSequence(this) { it.parentFile }.any { it.isHidden }
 
