3eebf5f015a94314dc9892919a65e2eed382e48d
Randy <drbugsmn@gmail.com>
2018-02-07 11:36:15 -0600

    SAMConstructors

--- src/ch05/5.4.2_SAMConstructors.kt
+++ src/ch05/5.4.2_SAMConstructors.kt
@@ -1,5 +1,16 @@
 package ch05.ex4_2_SAMConstructors
 
+
+/**
+ * SAM - Single Abstract Method inteface, same as a functional intefrace in java 8
+ *
+ * Kotlin will allow you to pass in lambdas to java methods that take functional intefaces as args
+ *
+ * From the book:
+ * A SAM constructor is a compiler-generated function that lets you perform an explicit conversion of
+ * a lambda into an instance of a functional interface.
+ *
+ */
 fun createAllDoneRunnable(): Runnable {
     return Runnable { println("All done!") }
 }
