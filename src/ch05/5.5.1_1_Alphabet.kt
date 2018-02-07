f4835ba2d8925247a144d3382cd1fad9f4d76082
Randy <drbugsmn@gmail.com>
2018-02-07 11:41:26 -0600

    With Function

--- src/ch05/5.5.1_1_Alphabet.kt
+++ src/ch05/5.5.1_1_Alphabet.kt
@@ -9,6 +9,34 @@ fun alphabet(): String {
     return result.toString()
 }
 
+//Note these are the '*_TheWithFunction 1 and 2
+fun lambdaAlphabet() : String {
+    val stringBuilder = StringBuilder()
+    return with(stringBuilder) { //specify the reveiver to get invoked in the 'with' block
+        for (letter in 'A'..'Z') {
+            this.append(letter) //calls a method on the reciever with the 'this'.  'this' appears optional in the block
+        }
+        append("\nNow I really know my alphabet") //calls a method without 'this'
+        toString() //another method on the reciever (can invoke this.toString also
+    }
+}
+
+//note expression body syntax
+fun alphabetWithAndAnExpressionBody() = with(StringBuilder()) {
+    for (letter in 'A'..'Z') {
+        append(letter)
+    }
+    append("\nNow I know another way to do the alphabet")
+    toString()
+}
+
+/**
+ * Note if there is a function/method name in the outer class that has the same name as something
+ * in the with receiver, you can use the following syntax to access the outer member
+ *     this@OuterClass.toString()
+ */
 fun main(args: Array<String>) {
     println(alphabet())
+    println(lambdaAlphabet())
+    println(alphabetWithAndAnExpressionBody())
 }
