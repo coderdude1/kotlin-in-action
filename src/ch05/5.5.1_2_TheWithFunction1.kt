ae4a8709483c82db17e4f75a83f47cf818582f57
Randy <drbugsmn@gmail.com>
2018-03-05 21:58:33 -0600

    with function and lambdas

--- src/ch05/5.5.1_2_TheWithFunction1.kt
+++ src/ch05/5.5.1_2_TheWithFunction1.kt
@@ -2,12 +2,14 @@ package ch05.ex5_1_2_TheWithFunction1
 
 fun alphabet(): String {
     val stringBuilder = StringBuilder()
+    //intellij appens this: Stringbuilder after the next line.
+    // with takes a function called the receiver ('stringBuilder') which can be referenced via 'this'
     return with(stringBuilder) {
         for (letter in 'A'..'Z') {
-            this.append(letter)
+            this.append(letter) //this refernces the art to with().  intellij hints that
         }
         append("\nNow I know the alphabet!")
-        this.toString()
+        this.toString() //note that intellij prepends ^with to indicate what up
     }
 }
 
--- src/ch05/5.5.1_3_TheWithFunction2.kt
+++ src/ch05/5.5.1_3_TheWithFunction2.kt
@@ -1,11 +1,13 @@
 package ch05.ex5_1_3_TheWithFunction2
 
+//expression syntax with the 'with' assinged to 'alphabet()' as the receiver
+//note this is a bit cleaner, less code than the previous with example
 fun alphabet() = with(StringBuilder()) {
     for (letter in 'A'..'Z') {
-        append(letter)
+        append(letter) //note no 'this'
     }
     append("\nNow I know the alphabet!")
-    toString()
+    toString() //intellij prepends '$with' in front
 }
 
 fun main(args: Array<String>) {
