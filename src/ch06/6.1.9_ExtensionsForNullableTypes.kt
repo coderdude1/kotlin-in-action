17e593f0cfcb15d45b927f2676c396fb08389d46
Randy <drbugsmn@gmail.com>
2018-03-11 10:52:51 -0500

    Extension functions and nulls

--- src/ch06/6.1.9_ExtensionsForNullableTypes.kt
+++ src/ch06/6.1.9_ExtensionsForNullableTypes.kt
@@ -1,6 +1,8 @@
 package ch06.ex1_9_ExtensionsForNullableTypes
 
+//note String input is nullable
 fun verifyUserInput(input: String?) {
+    //note no safe call on receiver, even tho it is nullable, extensions manage that for us
     if (input.isNullOrBlank()) {
         println("Please fill in the required fields")
     }
