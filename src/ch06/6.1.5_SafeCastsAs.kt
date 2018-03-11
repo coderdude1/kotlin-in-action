a58385eb3ad3cbc062132ebe6ee6c82a4681faba
Randy <drbugsmn@gmail.com>
2018-03-10 21:36:00 -0600

    Safe Casts

--- src/ch06/6.1.5_SafeCastsAs.kt
+++ src/ch06/6.1.5_SafeCastsAs.kt
@@ -1,7 +1,11 @@
 package ch06.ex1_5_SafeCastsAs
 
 class Person(val firstName: String, val lastName: String) {
+    //the Any? accepts any type, even null
    override fun equals(o: Any?): Boolean {
+
+        //o as? Person returns o if it is a person, otherwise returns null
+        //elvis, if o is a person set otherPerson to o, otherwise return false
       val otherPerson = o as? Person ?: return false
 
       return otherPerson.firstName == firstName &&
