73bbb77c7ec53f8ad8d2820f77660f991ccb8e5d
Randy <drbugsmn@gmail.com>
2018-03-13 22:45:05 -0500

    Nullability of params and generic

--- src/ch06/6.1.10_NullabilityOfTypeParameters.kt
+++ src/ch06/6.1.10_NullabilityOfTypeParameters.kt
@@ -1,9 +1,15 @@
 package ch06.ex1_10_NullabilityOfTypeParameters
 
-fun <T> printHashCode(t: T) {
+//Generics are the only time '?' is default allowing nulls in kotlin
+fun <T> printHashCodeNullable(t: T) { //T is inferred to be of type Any? (note '?' so null is valid)
+    println(t?.hashCode())
+}
+
+fun <T: Any> printHashCodeNotNullable(t: T) {
     println(t?.hashCode())
 }
 
 fun main(args: Array<String>) {
-    printHashCode(null)
+    printHashCodeNullable(null) //prints null as T is inferred from Any? note the '?'
+    //printHashCodeNotNullable(null) //compile error inferred type Nothing is not subtype of Any
 }
