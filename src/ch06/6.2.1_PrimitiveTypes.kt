69f37efa92d9746b4ad45623a59dc8732d9ba3c6
Randy <drbugsmn@gmail.com>
2018-03-22 19:29:21 -0500

    Primitivess and nullable primative types

--- src/ch06/6.2.1_PrimitiveTypes.kt
+++ src/ch06/6.2.1_PrimitiveTypes.kt
@@ -1,10 +1,11 @@
 package ch06.ex2_1_PrimitiveTypes
 
 fun showProgress(progress: Int) {
+    //coerceIn restricts the receiver to the range (in this ex 0, 100)
     val percent = progress.coerceIn(0, 100)
     println("We're ${percent}% done!")
 }
 
 fun main(args: Array<String>) {
-    showProgress(146)
+    showProgress(146) //returns 100
 }
--- src/ch06/6.2.2_NullablePrimitiveTypes.kt
+++ src/ch06/6.2.2_NullablePrimitiveTypes.kt
@@ -1,8 +1,9 @@
 package ch06.ex2_2_NullablePrimitiveTypes
 
 data class Person(val name: String,
-                  val age: Int? = null) {
+                  val age: Int? = null) { //name required, age nullable
 
+    //other not nullable, returns nullable Boolean, note kotlin uses java Boolean since its nullable
     fun isOlderThan(other: Person): Boolean? {
         if (age == null || other.age == null)
             return null
